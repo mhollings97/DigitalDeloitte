c6ddf82b8ca146049d265a3e6b999f41fb8afc5c
mhollings97 <michaelhollyt@gmail.com>
2018-05-29 07:52:05 -0400

    adding the simple home button

---
+++ my-app/src/components/Buttons/Home/HomeButton.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './HomeButton.css';
+class HomeButton extends Component {
+
+  render() {
+    return (
+      <div id = "ButtonWrapper">
+	<button id = "Button"> Deloitte Digital </button>
+    </div>
+
+    );
+  }
+}
+export default HomeButton;
