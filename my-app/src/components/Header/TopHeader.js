2d36a7fa6756c99d71c80d1495d257886393940b
sydneybecker <shb220@lehigh.edu>
2018-05-29 15:28:55 +0200

    Adding landing page header

---
+++ my-app/src/components/Header/TopHeader.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+import './TopHeader.css';
+import Header from '.././Buttons/Header/Header';
+import HomeButton from '.././Buttons/Home/HomeButton';
+
+class TopHeader extends Component { 
+
+    constructor(props) {
+	super(props);
+    }
+
+    render() {
+        return (
+        <div>
+       
+	<div id = "AU">
+	<Header title = "About Us" ></Header>
+	</div>
+	<div id = "AP">
+	<Header title = "About the Project"></Header>
+	</div>
+	<div id = "CU">
+	<Header title = "Contact Us"></Header>
+	</div>
+	<div id = "SI">
+	<Header title = "Sign In"></Header>
+	</div>
+
+	<div id = "home">
+	<HomeButton>Deloitte Digital</HomeButton>
+	</div>
+        </div>
+	)
+     }
+
+}
+
+export default TopHeader;
