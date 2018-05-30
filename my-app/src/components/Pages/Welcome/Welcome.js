d9f6583a4b85baa1fac7e8dfc8a85432252fe45c
mhollings97 <michaelhollyt@gmail.com>
2018-05-30 08:52:48 -0400

    adding welcome stuff

--- my-app/src/components/Pages/Welcome/Welcome.js
+++ my-app/src/components/Pages/Welcome/Welcome.js
@@ -4,6 +4,8 @@ import TopHeader  from '../.././Header/TopHeader'
 import GrayBox  from '../.././GrayBox/GrayBox'
 import TextBox from '../.././TextBox/TextBox'
 import './Welcome.css'
+
+
 class Welcome extends Component {
 
 constructor(props) {
@@ -14,11 +16,14 @@ constructor(props) {
 render() {
     return (
 	<div>
+		<TextBox size = "small" message = "What is this about?"/>
+		<TextBox size = "large" message = "Welcome to our new platform, our workplace for externam employees"/>
+
 		<div>
-			<TextBox size = "small" message = "What is this about?"/>
-		</div>
-		<div>
-			<TextBox size = "large" message = "Welcome to our new platform, our workplace for externam employees"/>
+			<div id = "LP">
+				<p>"This is our new project where we created a place for students and digital technologies enthusiasts to be able to work with us remotely on real projects and get experience, feedback and reward in return" </p>
+			</div>
+
 		</div>
 	</div>
 
