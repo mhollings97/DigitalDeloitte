b23a46e387c87333a4a3d90fe1814ef75949de5f
sydneybecker <shb220@lehigh.edu>
2018-05-31 09:40:39 +0200

    cute as a button

---
+++ my-app/src/components/Buttons/Apply/Apply.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import './Apply.css';
+
+class Apply extends Component {
+
+
+    render() {
+
+	return (
+	    <div id = "Apply">
+		<button id = "VirtualInterview">APPLY FOR VIRTUAL INTERVIEW</button>
+	    </div>
+	);
+
+
+    }
+
+
+}
+
+export default Apply;
