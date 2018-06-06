28cda3e9b73abd9b28ae017163cc6f5ebe6cd8e6
mhollings97 <michaelhollyt@gmail.com>
2018-06-06 08:28:56 -0400

    user profile beginings

---
+++ my-app/src/components/UserProf/UserProf.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import './UserProf.css';
+class UserProf extends Component {
+
+ constructor() {
+                super();
+                this.state = {
+			firstName: "",
+			lastName: "",
+			role: ""
+
+                };
+
+}
+componentDidMount() {
+
+
+fetch('https://private-ae364-vdwregistration.apiary-mock.com/api/v1/people/1', 
+	{
+	method:"GET",
+	headers: {
+ 		"Content-Type": "application/json"
+ 	 }})
+
+	.then(results => {
+                return results.json();
+        })
+
+	.then(responseData => {
+		console.log(responseData.data);
+
+			this.setState({
+			firstName: responseData.data.firstName,
+			role: responseData.data.role,
+			lastName: responseData.data.lastName,
+		});
+
+        })
+
+
+
+}
+
+
+  render() {
+    return (
+
+	<div>
+		<p>{this.state.firstName}</p>
+		<p>{this.state.lastName} </p>
+		<p>{this.state.role} </p>
+	</div>
+
+    );
+  }
+}
+export default UserProf;
+
+
+
+
+
+
+
