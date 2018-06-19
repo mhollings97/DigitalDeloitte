0ee28b30a0e54222932917b5f5bf1758769055db
Max Urban <mru220@lehigh.edu>
2018-06-19 15:28:20 +0200

    enhanced the aesthetics of the project detail page

--- my-app/src/components/Pages/ProjPage/ProjPage.js
+++ my-app/src/components/Pages/ProjPage/ProjPage.js
@@ -13,7 +13,7 @@ render () {
 	<div>
 	  <div id = "bigwrap">
 	      <div id = "projinfo">
-		<p>We compete for the best graphic design poster for the movie "Futuristic Vision"</p>
+		<p><h2>We compete for the best graphic design poster for the movie "Futuristic Vision"</h2></p>
 		<p>Your task will be to design one or at most three variants of a 100x70 cm film poster. The poster will be dedicated to the film exhibition and will be used to present an unprecedented film called "Next Future". The film deals with a vision of the future where there will be a disproportionate development of new technologies that will fundamentally transform our whole world as we know it.</p>
 		<p>This is a medium project, so it will be necessary to submit at least one revision and the incorporation of the changes, before the submission of the contest, where from the maximum of five participants we will select the winner who will get the maximum reward. Those who fail will not come and receive a reward if they successfully fulfill the conditions and submit their project.</p>
 		<p>The project has a reject date of 31.01.2018 that can not be moved.</p>
@@ -24,7 +24,7 @@ render () {
 	        <ProjDescription/>
 	      </div>
 	      <div id = "startwork">
-		<button id = "startworking">Start working</button>
+		<button id = "startworking"><b>Start working</b></button>
 	      </div>
 	  </div>
 	</div>
--- my-app/src/components/ProjDescription/ProjDescription.js
+++ my-app/src/components/ProjDescription/ProjDescription.js
@@ -19,24 +19,24 @@ render () {
 	    <p>Small</p>
 	    <p>Competitive</p>
 	  </div>
-
+	<hr></hr>
 	  <div id = "time">
 	    <h3>Estimated time effort</h3>
 	    <p>6-8 weeks</p>
 	  </div>
-
+	<hr></hr>
 	  <div id = "deadlines">
 	    <h3>Deadlines</h3>
 	    <p>Joining: 06.01.2018</p>
 	    <p>Revision: 20.01.2018</p>
 	    <p>Submission: 31.01.2018</p>
 	  </div>
-
+	<hr></hr>
 	  <div id = "requirements">
 	    <h3>Requirements</h3>
-	    <p>if any</p>
+	    <p>Advanced Miming</p>
 	  </div>
-
+	<hr></hr>
 	  <div id = "reward">
 	    <h3>Reward</h3>
 	    <p>Winner: 50 ExP</p>
