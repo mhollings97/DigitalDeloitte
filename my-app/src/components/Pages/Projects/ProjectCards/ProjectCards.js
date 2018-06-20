cbc6d808d8ca364e35e273ff54dc0afd38dcef07
Evan Choy <ChessWCM888@MacBook-Pro.local>
2018-06-20 05:37:09 -0400

    I hate everything. Trying to make dynamic cards

--- my-app/src/components/Pages/Projects/ProjectCards/ProjectCards.js
+++ my-app/src/components/Pages/Projects/ProjectCards/ProjectCards.js
@@ -6,61 +6,50 @@ class ProjectCards extends Component {
     constructor (props) {
 	super(props);
 
-	this.state = { myProjects: [] }
+	this.state = {
+	    id: [],
+	    loading: 'initial'
+	}
     }
 
+    componentDidMount() {
+	this.setState({loading: 'true'})
+	    fetch('http://localhost:3050/api/v1/project/' + sessionStorage.getItem('user_id'),
+		  {
+		      method:"GET",
+			  headers: {
+			  "Content-Type": "application/json"
+			      }})
 
-    componentWillMount() {
-
-
-	fetch('localhost:3050/api/v1/project/' + sessionStorage.getItem('user_id'),
-	      {
-		  method:"GET",
-		      headers: {
-		      "Content-Type": "application/json"
-			  }})
-
-	    .then(results => {
-		    return results.json();
-		})
-
-	    .then(ret => {
-		    var projects = ret.data.projectData;
-		    
-		    this.setState({ myProjects: projects });
+	    .then((res) => res.json())
+	    .then(responseData => {
+		    console.log(responseData);
+		    var temp = [];
+		    for(var i = 0; i < responseData.data.projectData.length; i++) {
+                temp[i] = responseData.data.projectData[i].project_id
+		    }
+		    this.setState({id: temp, loading: 'false'})
 		})
 	    }
-    
     render () {
-		/*
-	<div>
-	<div id = "AllCards">
 
-	    <div id = "PC1">
-		<ProjCard/>
-	    </div>
+        if(this.state.loading === 'initial') {
+            return <h2></h2>
+		}
+        if(this.state.loading === 'true') {
+            return <h2></h2>
+		}
 
-	    <div id = "PC2">
-		<ProjCard/>
-	    </div>
-
-	    <div id = "PC3">
-		<ProjCard/>
-	    </div>
-
-	    <div id = "PC4">
-		<ProjCard/>
-	    </div>
-
-	</div>
-	</div>
-		*/
-		var container = document.getElementById("container");
-		container.innerHTML += '<div class = "wrapper">';
+                var container = document.getElementById("container");
+		container.innerHTML += '<div>';
+		container.innerHTML += '<div id = "AllCards">';
 		for(var i = 0; i < this.state.myProjects.length; i++)
 		    {
-			container.innerHTML += '<div id = "Project' + i + '"><ProjCard/></div>';
+			container.innerHTML += '<div id = "Project' + i +
+			    '"><ProjCard id = {' this.state.id[i] +
+			    '}/></div>';
 		    }
+		container.innerHTML += '</div';
 		container.innerHTML += '</div';
 		return container;
 	}
